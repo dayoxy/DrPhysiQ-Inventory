90cd7c90c94bea5ca301b01a7454f01411ce6f45
Adedayo Michael Oguntonade <Adedayom70@gmail.com>
2026-01-05 20:06:16 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -20,11 +20,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # ================= PASSWORD =================
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
+
 
 def verify_password(plain_password: str, hashed_password: str):
     print("PASSWORD LENGTH:", len(plain_password))
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 # ================= TOKEN =================
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
