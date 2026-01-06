aa106705cd75d17caf701f3c348814bb2ef2924d
Adedayo Michael Oguntonade <Adedayom70@gmail.com>
2026-01-06 01:39:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "https://drphysi.netlify.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
